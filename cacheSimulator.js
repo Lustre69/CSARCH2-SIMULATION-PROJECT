35b413fcb98b3516b3247bd677b668421f57ad1d
paolofloress <95564127+paolofloress@users.noreply.github.com>
2023-11-27 21:10:09 +0800

    Add files via upload

--- cacheSimulator.js
+++ cacheSimulator.js
@@ -1,106 +1,105 @@
 class CacheSimulator {
-    constructor(cacheBlocks = 32, cacheLineSize = 16, ways = 8) {
+    constructor(cacheBlocks = 32, ways = 8) {
         this.cacheBlocks = cacheBlocks;
-        this.cacheLineSize = cacheLineSize;
         this.ways = ways;
         this.sets = cacheBlocks / ways;
-        this.cache = new Array(this.sets).fill(null).map(() => new Array(this.ways).fill(null));
-        this.mru = new Array(this.sets).fill(null).map(() => new Array(this.ways).fill(-1));
-        this.memoryAccessCount = 0;
-        this.cacheHitCount = 0;
-        this.cacheMissCount = 0;
+        this.cache = this.initializeCache();
+        this.mru = this.initializeMRU();
+        this.logs = [];
+    }
+
+    initializeCache() {
+        return new Array(this.sets).fill(null).map(() => new Array(this.ways).fill(null));
+    }
+
+    initializeMRU() {
+        return new Array(this.sets).fill(null).map(() => new Array(this.ways).fill(0));
     }
 
     accessMemory(blockAddress) {
-        this.memoryAccessCount++;
         const setIndex = blockAddress % this.sets;
-        const wayIndex = this.findWayIndex(setIndex, blockAddress);
-        
-        if (wayIndex !== -1) {
-            // Cache hit
-            this.cacheHitCount++;
-            this.updateMRU(setIndex, wayIndex);
+        const wayIndex = this.cache[setIndex].indexOf(blockAddress);
+
+        if (wayIndex !== -1) { // Cache hit
+            this.updateMRU(setIndex, wayIndex, true);
+            this.logs.push(`Access: ${blockAddress} (Hit)`);
+        } else { // Cache miss
+            const replaceIndex = this.mru[setIndex].indexOf(Math.min(...this.mru[setIndex]));
+            this.cache[setIndex][replaceIndex] = blockAddress;
+            this.updateMRU(setIndex, replaceIndex, false);
+            this.logs.push(`Access: ${blockAddress} (Miss)`);
+        }
+    }
+
+    updateMRU(setIndex, wayIndex, isHit) {
+        if (isHit) {
+            this.mru[setIndex][wayIndex]++;
         } else {
-            // Cache miss
-            this.cacheMissCount++;
-            this.addToCache(setIndex, blockAddress);
+            this.mru[setIndex] = this.mru[setIndex].map((val, idx) => (idx === wayIndex ? 1 : val + 1));
         }
     }
 
-    findWayIndex(setIndex, blockAddress) {
-        return this.cache[setIndex].findIndex(block => block === blockAddress);
-    }
-
-    updateMRU(setIndex, wayIndex) {
-        this.mru[setIndex][wayIndex] = this.memoryAccessCount;
-        // Update MRU order for the set
-        this.mru[setIndex].sort((a, b) => b - a);
-    }
-
-    addToCache(setIndex, blockAddress) {
-        const leastRecentlyUsedIndex = this.mru[setIndex].lastIndexOf(Math.min(...this.mru[setIndex]));
-        this.cache[setIndex][leastRecentlyUsedIndex] = blockAddress;
-        this.updateMRU(setIndex, leastRecentlyUsedIndex);
-    }
-
-    runTest(testType, n) {
-        if (testType === 'sequential') {
-            for (let i = 0; i < 4; i++) {
-                for (let blockAddress = 0; blockAddress < 2 * n; blockAddress++) {
-                    this.accessMemory(blockAddress);
-                }
+    runTest(testType, n, traceType) {
+        this.resetCache();
+        const sequence = this.generateSequence(testType, n);
+        sequence.forEach((blockAddress, index) => {
+            this.accessMemory(blockAddress);
+            if (traceType === 'stepByStep') {
+                this.displayCurrentState(index + 1);
             }
-        } else if (testType === 'random') {
-            for (let i = 0; i < 4 * n; i++) {
-                const blockAddress = Math.floor(Math.random() * 2 * n);
-                this.accessMemory(blockAddress);
-            }
-        } else if (testType === 'midRepeat') {
-            const sequence = [];
-            for (let i = 0; i < n; i++) {
-                sequence.push(i);
-            }
-            sequence.push(...sequence); // Repeat middle part of sequence
-            for (let i = n; i < 2 * n; i++) {
-                sequence.push(i);
-            }
-            for (let i = 0; i < 4; i++) {
-                sequence.forEach(blockAddress => {
-                    this.accessMemory(blockAddress);
-                });
-            }
+        });
+        if (traceType === 'finalSnapshot') {
+            this.displayCurrentState(sequence.length);
         }
+        this.displayLogs();
     }
 
-    getStatistics() {
-        const hitRate = this.cacheHitCount / this.memoryAccessCount;
-        const missRate = this.cacheMissCount / this.memoryAccessCount;
-        return {
-            memoryAccessCount: this.memoryAccessCount,
-            cacheHitCount: this.cacheHitCount,
-            cacheMissCount: this.cacheMissCount,
-            cacheHitRate: hitRate,
-            cacheMissRate: missRate
-        };
+    resetCache() {
+        this.cache = this.initializeCache();
+        this.mru = this.initializeMRU();
+        this.logs = [];
+    }
+
+    generateSequence(testType, n) {
+		let sequence = [];
+		switch (testType) {
+			case 'sequential':
+				sequence = Array.from({ length: 2 * n }, (_, i) => i % n);
+				break;
+			case 'random':
+				for (let i = 0; i < 4 * n; i++) {
+					sequence.push(Math.floor(Math.random() * n));
+				}
+				break;
+			case 'midRepeat':
+				let half = Array.from({ length: n }, (_, i) => i);
+				sequence = [...half, ...half, ...Array.from({ length: n }, (_, i) => i + n)];
+				break;
+			default:
+				sequence = Array.from({ length: n }, (_, i) => i);
+    }
+    return sequence.flat().slice(0, 4 * n); // Ensure the sequence is 4n in length
+}
+
+    displayCurrentState(step) {
+		const outputDiv = document.getElementById('output');
+		let outputHtml = `<h3>Step ${step}</h3>`;
+		this.cache.forEach((set, setIndex) => {
+			outputHtml += `<div>Set ${setIndex}: [${set.join(', ')}]</div>`;
+		});
+    outputDiv.innerHTML = outputHtml;
+}
+
+    displayLogs() {
+        const logDiv = document.getElementById('log');
+        logDiv.innerHTML = this.logs.join('<br>');
     }
 }
 
 function runSimulation() {
     const testType = document.getElementById("testType").value;
     const cacheBlocks = parseInt(document.getElementById("cacheBlocks").value);
+    const traceType = document.getElementById("traceType").value;
     const simulator = new CacheSimulator(cacheBlocks);
-    simulator.runTest(testType, cacheBlocks);
-    const statistics = simulator.getStatistics();
-    displayResults(statistics);
-}
-
-function displayResults(stats) {
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = `
-        <p>Memory Access Count: ${stats.memoryAccessCount}</p>
-        <p>Cache Hit Count: ${stats.cacheHitCount}</p>
-        <p>Cache Miss Count: ${stats.cacheMissCount}</p>
-        <p>Cache Hit Rate: ${stats.cacheHitRate.toFixed(2)}</p>
-        <p>Cache Miss Rate: ${stats.cacheMissRate.toFixed(2)}</p>
-    `;
+    simulator.runTest(testType, cacheBlocks, traceType);
 }
